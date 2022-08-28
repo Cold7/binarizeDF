e15bdfb737adc45dfcfea1da131cedf1f9c8a8b7
Cold7 <contrerasriquelme.sebastian@gmail.com>
2022-08-28 17:28:58 -0400

    solved bug when using threshold higher than 0

--- binarizeDF.py
+++ binarizeDF.py
@@ -1,5 +1,6 @@
 #binarizer function
 def binarize(df, threshold=0):
     for col in df.columns:
+        df2.loc[df2[col] < threshold, col] = 0
         df.loc[df[col] > threshold, col] = 1
     return df
