f0ecf235e45fed5981b3931a85490480307c7f4c
Cold7 <contrerasriquelme.sebastian@gmail.com>
2022-11-22 12:43:12 -0300

    Update binarizeDF.py

--- binarizeDF.py
+++ binarizeDF.py
@@ -1,6 +1,7 @@
 #binarizer function
 def binarize(df, threshold=0):
+    df2 = df.copy()
     for col in df.columns:
         df2.loc[df2[col] < threshold, col] = 0
-        df.loc[df[col] > threshold, col] = 1
-    return df
+        df2.loc[df2[col] > threshold, col] = 1
+    return df2
